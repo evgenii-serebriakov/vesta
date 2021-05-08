652d17bd7d06ace793551d7dec5920697baad5e2
Evgenii <litroboll@gmail.com>
2021-05-08 23:42:47 +0500

    Added a single post page

--- resources/assets/js/pages/SinglePost.vue
+++ resources/assets/js/pages/SinglePost.vue
@@ -1,18 +1,23 @@
 <template lang="pug">
 section.post.uk-section
-    .uk-container
-        .uk-flex.uk-flex-wrap
-            .uk-width-1-1(v-if="post.image")
-                .post__image
-                    img(:src="post.image", :alt="post.alt")
+    article.uk-article
+        .uk-container.uk-container-small
+            .uk-flex.uk-flex-wrap.uk-flex-center
+                .uk-width-1-1(v-if="post.image")
+                    .post__image
+                        img(:src="post.image", :alt="post.alt")
 
-            .uk-width-1-1
-                .uk-card
-                    .uk-card-body
-                        h2.post__caption.uk-text-uppercase {{ post.title }}
-                        p.post__meta-info {{ post.date }}
+                .uk-width-1-1
+                    .uk-card.uk-card-default.uk-card-hover
+                        .uk-card-body
+                            h2.post__caption.uk-text-uppercase.uk-margin-remove.decorating-line  {{ post.title }}
+                            p.post__meta-info.uk-text-meta
+                                time(
+                                    datetime="2004-07-24T18:18"
+                                    aria-label="Date of publication"
+                                ) {{ post.date }}
 
-                        p.post__message {{ post.message }}
+                            p.post__message.uk-text-break {{ post.message }}
 </template>
 
 <script>
@@ -25,7 +30,7 @@ export default {
         const post = computed(() => state.getters.post);
 
         onMounted(() => {
-            console.log(post.value);
+            // console.log(post.value);
         });
 
         return {
@@ -34,3 +39,30 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "@/scss/utils/_variables";
+
+@import "@/scss/mixins/_media";
+
+.post {
+    &__image {
+        margin-bottom: rem(40);
+    }
+
+    &__caption {
+        position: relative;
+        font-size: 28px;
+
+        &::before {
+            top: 59px;
+            background: #e5e5e5;
+        }
+    }
+
+    &__message {
+        padding-top: rem(40);
+        font-size: 26px;
+    }
+}
+</style>
--- resources/assets/js/pages/SingleVideo.vue
+++ resources/assets/js/pages/SingleVideo.vue
@@ -1,39 +1,39 @@
 <template lang="pug">
 section.single-video.uk-section
-    .uk-container
-        .uk-flex.uk-flex-wrap
-            .uk-width-1-1(class="uk-width-1-2@m")
-                .single-video__box
-                    transition(name="fade" mode="in-out")
-                        img(
-                            v-if="hasChanges"
-                            class="single-video__img"
-                            :src="video.image",
-                            :alt="video.alt"
+    article.uk-article
+        .uk-container
+            .uk-flex.uk-flex-wrap
+                .uk-width-1-1(class="uk-width-1-2@m")
+                    .single-video__box
+                        transition(name="fade" mode="in-out")
+                            img(
+                                v-if="hasChanges"
+                                class="single-video__img"
+                                :src="video.image",
+                                :alt="video.alt"
+                            )
+                        iframe(
+                            class="single-video__frame"
+                            :src="video.video"
+                            width="100%"
+                            height="315"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                            allowfullscreen
+                            @load="onFrameLoading"
                         )
-                    iframe(
-                        class="single-video__frame"
-                        :src="video.video"
-                        width="100%"
-                        height="315"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowfullscreen
-                        @load="onFrameLoading"
-                    )
 
-            .uk-width-1-1(class="uk-width-1-2@m")
-                .single-video__card.uk-card.uk-card-default.uk-card-hover.uk-margin
-                    .uk-card-body
-                        h3.single-video__caption.uk-card-title.uk-text-uppercase {{ video.title }}
-                        p.single-video__meta-info.uk-text-meta
-                            time(
-                                datetime="2004-07-24T18:18"
-                                aria-label="Date of publication"
-                            ) {{ video.date }}
-                        p.single-video__text.uk-text-break {{ video.message }}
-
+                .uk-width-1-1(class="uk-width-1-2@m")
+                    .single-video__card.uk-card.uk-card-default.uk-card-hover.uk-margin
+                        .uk-card-body
+                            h2.single-video__caption.uk-card-title.uk-text-uppercase {{ video.title }}
+                            p.single-video__meta-info.uk-text-meta
+                                time(
+                                    datetime="2004-07-24T18:18"
+                                    aria-label="Date of publication"
+                                ) {{ video.date }}
+                            p.single-video__text.uk-text-break {{ video.message }}
 </template>
 
 <script>
@@ -79,7 +79,7 @@ export default {
 
     &__caption {
         margin-bottom: 0;
-        font-size: 26px;
+        font-size: 28px;
     }
 
     &__meta-info {
@@ -87,7 +87,7 @@ export default {
     }
 
     &__text {
-        font-size: 25px;
+        font-size: 26px;
     }
 
     @include media-breakpoint(960) {
